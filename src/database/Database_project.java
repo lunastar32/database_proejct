29e8f9fb305ba4f1f817f33ead0f50e47c79810b
lunastar32 <artangel00504@gmail.com>
2025-05-22 11:23:47 +0900

    기본 세팅 업로드

---
+++ src/database/Database_project.java
@@ -0,0 +1,15 @@
+package database;
+
+public class Database_project {
+
+	public static void 초기화면() {
+		System.out.println("케이크 주문 시스템");
+		System.out.println();
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		초기화면();
+		
+	}
+
+}
