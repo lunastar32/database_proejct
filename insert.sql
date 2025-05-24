664d02f6ba888f0f51b258f64b97c4bc6a533cd1
boolynn17 <166924043+boolynn17@users.noreply.github.com>
2025-05-25 03:18:58 +0900

    Delete insert.sql

--- insert.sql
+++
@@ -1,5 +0,0 @@
--- 주문 추가
-INSERT INTO orders (order_id, customer_id, order_date) VALUES (?, ?, CURRENT_DATE);
-
--- 고객 정보 추가
-INSERT INTO customer(customer_id, customer_name, phone_number) VALUES (?, ?, ?);
