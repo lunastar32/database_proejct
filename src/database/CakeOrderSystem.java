8f2f39e63989380224ff192b7848d75b65cd6b57
lunastar32 <artangel00504@gmail.com>
2025-05-27 18:02:48 +0900

    5/27
    함수 추가 및 내용 작성

--- src/database/CakeOrderSystem.java
+++ src/database/CakeOrderSystem.java
@@ -1,6 +1,7 @@
 package database;
 
 import java.sql.*;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class CakeOrderSystem {
@@ -17,7 +18,6 @@ public class CakeOrderSystem {
 	public static void 메뉴() {
 		Scanner scanner = new Scanner(System.in);
 		
-		System.out.println("[메뉴를 선택하세요]");
 		System.out.println("1. 케이크 주문하기");
 		System.out.println("2. 전체 주문 목록 조회");
 		System.out.println("3. 현재 주문 내역 조회");
@@ -27,10 +27,22 @@ public class CakeOrderSystem {
 		System.out.println();
 		System.out.println("0. 프로그램 종료");
 		
-		int choice = scanner.nextInt();
+		int choice = -1;
+
+		while(true){
+			try{
+			System.out.print("[메뉴를 선택하세요] : ");
+			choice = Integer.parseInt(scanner.nextLine());
+			break;
+			} catch(NumberFormatException e){
+				System.out.println("[오류] 숫자를 입력해주세요.\n");
+				continue;
+			}
+		}
 		
 		switch (choice) {
         case 1: // 케이크 주문하기
+					addOrder();
         	break;
         case 2: // 전체 주문 목록 조회
         	System.out.println("이름을 입력해주세요: ");
@@ -55,6 +67,25 @@ public class CakeOrderSystem {
 		}
 	}
 	
+	private static void addOrder(){
+		Scanner sc = new Scanner(System.in);
+    System.out.print("주문 ID: ");
+    String orderId = sc.nextLine();
+    System.out.print("고객 ID: ");
+    String customerId = sc.nextLine();
+    System.out.print("주문 날짜 (YYYY-MM-DD): ");
+    String date = sc.nextLine();
+
+    String query = "INSERT INTO orders (orders_id, customer_id, orders_date) VALUES (?, ?, ?)";
+		//    try (Connection conn = DBConnection.getConnection(); PreparedStatement pstmt = conn.prepareStatement(query)) {
+    //    pstmt.setString(1, orderId);
+  	//    pstmt.setString(2, customerId);
+    //    pstmt.setDate(3, Date.valueOf(date));
+    //    pstmt.executeUpdate();
+    //    System.out.println("주문이 추가되었습니다.");
+        }	
+	}
+
 	private static void updateCustomer() {
 		// TODO Auto-generated method stub
 		
